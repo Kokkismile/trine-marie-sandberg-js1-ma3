3c426f8192413bbbe539213162aa86b4a1017bed
Trine Sandberg <trine.marie.sandberg@gmail.com>
2022-04-22 23:21:04 +0200

    working on errorhandling

--- question-2.js
+++ question-2.js
@@ -4,6 +4,7 @@ const apiKey = "7e4f8935456c47bbb745861dedea99ef";
 const apiUrl = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=";
 
 htmlContainer = document.querySelector(".container");
+const errorhandling = document.querySelector(".message");
 let objects = "";
 
 async function apiCall() {
@@ -12,6 +13,9 @@ async function apiCall() {
         
         const returnJson = await response.json();
         console.log(returnJson.results)
+
+        errorhandling.innerHTML = displaySuccess("Success!");
+
         for (i = 0; i < 8; i++){
             console.log([returnJson.results[i]])
 
@@ -23,21 +27,9 @@ async function apiCall() {
 
         } catch (error) {
         console.log(error);
-        htmlContainer.innerHTML = error;
+        errorhandling.innerHTML = displayError("Error");
         }       
     
 };
 
-
 apiCall()
-
-const errorhandling = document.querySelector(".success-error-message");
-
-try {
-    getSomething();
-    errorhandling.innerHTML = displaySuccess("Success!");
-} catch(error) {
-    console.log("ERROR:", error);
-    //prints this message to page using premade componments and styling.
-    errorhandling.innerHTML = displayError("Error");
-}
